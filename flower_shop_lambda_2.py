ce4a4576c0b6e51bff8961121e6b6cd54e86df17
Brian Murray <murraycoding@gmail.com>
2023-05-09 13:44:27 -0400

    Update flower_shop_lambda_2.py

--- flower_shop_lambda_2.py
+++ flower_shop_lambda_2.py
@@ -27,8 +27,6 @@ def lambda_handler(event, context):
     logger.info(event)
     http_method = event['httpMethod']
     path = event['path']
-    request_body = json.loads(event['body'])
-    flower_id = build_flower_id(request_body)
 
     # health api endpoint
     if http_method == get_method and path == health_path:
@@ -39,23 +37,43 @@ def lambda_handler(event, context):
         print('DEBUG: The API is getting information on all of the flowers')
         response = get_flowers()
 
+    # sale endpoint
+    elif http_method == post_method and path == sale_path:
+        print("DEBUG: The API is making a sale to a customer")
+        request_body = json.loads(event['body'])
+        response = make_sale(request_body)
 
-    elif flower_id == None:
-        build_response(404, 'Flower ID should be built in the response body')
+    # purchase endpoint
+    elif http_method == post_method and path == purchase_path:
+        print("DEBUG: The API is making a purchase of flowers to add to the inventory")
+        request_body = json.loads(event['body'])
+        response = make_puchase(request_body)
 
     # flower end point
     elif http_method == get_method and path == flower_path:
         print('DEBUG: Lambda hits the get_flower')
+        request_body = json.loads(event['body'])
+        flower_id = build_flower_id(request_body)
         response = get_flower(flower_id)
     elif http_method == delete_method and path == flower_path:
         print('DEBUG: Lmabda hits the delete flower method')
+        request_body = json.loads(event['body'])
+        flower_id = build_flower_id(request_body)
         response = remove_flower(flower_id)
     elif http_method == post_method and path == flower_path:
         print("DEBUG: The API is creating a new flower.")
+        request_body = json.loads(event['body'])
+        flower_id = build_flower_id(request_body)
         response = add_flower(flower_id, request_body)
     elif http_method == put_method and path == flower_path:
         print("DEBUG: The API is editing a flower.")
+        request_body = json.loads(event['body'])
+        flower_id = build_flower_id(request_body)
         response = update_flower(flower_id, request_body)
+    else:
+        response = build_response(404, "Error")
+    
+    return response
 
 
 def build_response(status_code, body=None):
@@ -74,60 +92,253 @@ def build_flower_id(request_body):
     try:
         flower_name = request_body['name']
         flower_color = request_body['color']
-        return f'{flower_color}_{flower_name}'
+        return str(f'{flower_color}_{flower_name}')
 
     except:
         return None
 
 def get_flower(flower_id):
-    pass
+    try:
+        print(isinstance(flower_id,str))
+        print(isinstance(flower_id,dict))
+        response = client.get_item(
+            TableName = flower_inventory_table_name,
+            Key = {
+                'flower_id': {
+                    'S': flower_id
+                }
+            }
+        )
+
+        body = {
+            'Operation': 'Get Flower',
+            'Message': 'Success',
+            'Item': response['Item']
+        }
+
+        return build_response(200,body)
+    except:
+        logger.exception("Cannot find flower information.")
+    
 
 def remove_flower(flower_id):
-    pass
+    try:
+        response = client.delete_item(
+            TableName = flower_inventory_table_name,
+            Key = {
+                'flower_id': {
+                    'S': flower_id
+                }
+            }
+        )
+
+        body = {
+            'Operation': 'Remove Flower',
+            'Message': 'Success'
+        }
+
+        return build_response(200, body)
+    except:
+        logger.exception("Can't remove the flower")
 
 def add_flower(flower_id, request_body):
-    pass
+    try:
+        name = request_body['name']
+        color = request_body['color']
+        price = request_body['price']
+        quantity = request_body['quantity']
+
+        # used for debugging
+        print(f'Flower ID = {flower_id}')
+        print(f'Name = {name}')
+        print(f'Color = {color}')
+        print(f'Price = {price}')
+        print(f'Quantity = {quantity}')
+        
+        response = client.update_item(
+            
+        TableName = flower_inventory_table_name,
+        
+        ExpressionAttributeNames = {
+            '#N': 'name',
+            '#C': 'color',
+            '#P': 'price',
+            '#Q': 'quantity'
+        },
+        ExpressionAttributeValues = {
+            ':n': {
+                'S': name 
+            },
+            ':c': {
+                'S': color
+            },
+            ':p': {
+                'N': price
+            },
+            ':q': {
+                'N': quantity
+            }
+        },
+        Key = {
+            'flower_id': {
+                'S': flower_id
+            }
+        },
+        ReturnValues = 'ALL_NEW',
+        UpdateExpression = 'SET #N = :n, #C = :c, #P = :p, #Q = :q'
+        )
+        
+        print(response)
+        
+        body = {
+            'Message': 'Still Good!' # update this to include more information
+        }
+        
+        return build_response(200, body)
+
+    except:
+        logger.exception("There is a problem here.")
+    
 
 def update_flower(flower_id, request_body):
-    pass
+    try:
+        name = request_body['name']
+        color = request_body['color']
+        price = request_body['price']
+        quantity = request_body['quantity']
+        
+        # used for debugging
+        print(f'Flower ID = {flower_id}')
+        print(f'Name = {name}')
+        print(f'Color = {color}')
+        print(f'Price = {price}')
+        print(f'Quantity = {quantity}')
+        
+        response = client.update_item(
+            
+            TableName = flower_inventory_table_name,
+            
+            ExpressionAttributeNames = {
+                '#N': 'name',
+                '#C': 'color',
+                '#P': 'price',
+                '#Q': 'quantity'
+            },
+            ExpressionAttributeValues = {
+                ':n': {
+                    'S': name 
+                },
+                ':c': {
+                    'S': color
+                },
+                ':p': {
+                    'N': price
+                },
+                ':q': {
+                    'N': quantity
+                }
+            },
+            Key = {
+                'flower_id': {
+                    'S': flower_id
+                }
+            },
+            ReturnValues = 'ALL_NEW',
+            UpdateExpression = 'SET #N = :n, #C = :c, #P = :p, #Q = :q'
+        )
+        
+        print(response)
+        
+        body = {
+            'Message': 'Still Good!' # update this to include more information
+        }
+        
+        return build_response(200, body)
+
+    except:
+        logger.exception("There is a problem here.")
 
 def get_flowers():
     try:
         response = client.scan(
             TableName = flower_inventory_table_name,
             ExpressionAttributeNames = {
-                '#FN': 'flower_name',
-                '#FC': 'flower_color'
+                '#N': 'name',
+                '#C': 'color'
             },
-            ProjectionExpression = '#FN, #FC'
+            ProjectionExpression = '#N, #C'
         )
         result = response['Items']
 
         while 'LastEvaluatedKey' in response:
             response = client.scan(
                 TableName = flower_inventory_table_name,
+                ExclusiveStartKey = response['LastEvaluatedKey'],
                 ExpressionAttributeNames = {
-                '#FN': 'flower_name',
-                '#FC': 'flower_color'
+                '#N': 'name',
+                '#C': 'color'
                 },
-                ProjectionExpression = '#FN, #FC'
+                ProjectionExpression = '#N, #C'
             )
             result.extend(response['Items'])
     
         body = {
             'flowers': result
         }
+
+        return build_response(200, body)
     
     except:
         print("There is a mistake there")
         return None
 
 
+def make_sale(request_body):
+    pass
+
+def make_puchase(request_body):
+    # separates out each item
+    for item in request_body['Items']:
+        flower_id = build_flower_id(item)
+        print(f'Flower ID = {flower_id}')
+
+        # API will try to get the flower
+        try:
+            response = client.get_item(
+                Key = {
+                    'flower_id': {
+                        'S': flower_id
+                    }
+                }
+            )
+
+            # if the flower already exists, keep the information and add the quantity
+            update_response = update_flower(
+                flower_id = flower_id,
+                request_body = {
+                    'name': item['name'],
+                    'color': item['color'],
+                    'price': response['Item']['price'],
+                    'quantity': str(int(response['Item']['quantity']) + int(item['quantity'])) 
+                }
+            )
+
+        # flower does not exist, need to make a new table entry - price = 0
+        except:
+            response = add_flower(
+                flower_id = flower_id,
+                request_body = {
+                    'name': item['name'],
+                    'color': item['color'],
+                    'price': '0',
+                    'quantity': item['quantity']
+                }
+            )
+
+    body = {
+        'Operation': 'Purchase',
+        'Message': 'Success',
+    }
     
-
-def make_sale():
-    pass
-
-def make_puchase():
-    pass
+    return build_response(200, body)
 
