3993d0b37810fb4366d4d3ac961d241f8cbe42a9
Brian Murray <murraycoding@gmail.com>
2023-07-26 18:36:53 -0400

    started to update lambda functions

--- flower-shop/src/handlers/flowers.py
+++ flower-shop/src/handlers/flowers.py
@@ -6,7 +6,7 @@ client = boto3.client('dynamodb')
 
 def get_flower_information(event, context):
     if event["httpMethod"] != "GET":
-        raise Exception(f"getAllItems only accept GET method, you tried: {event.httpMethod}")
+        raise Exception(f"Flower and Flowers API endpoints only accept the GET method, you tried: {event.httpMethod}")
 
     data = client.scan(TableName=os.environ["SAMPLE_TABLE"])
     items = data["Items"]
