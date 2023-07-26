4018373ae85347cc07bdbee3d98fcbdb21205bfd
Brian Murray <murraycoding@gmail.com>
2023-07-26 12:10:57 -0400

    updated sam template with all custom resources

--- flower-shop/src/handlers/update_inventory.py
+++ flower-shop/src/handlers/update_inventory.py
@@ -4,7 +4,7 @@ import boto3
 
 client = boto3.client('dynamodb')
 
-def get_flower_information(event, context):
+def update_inventory_func(event, context):
     if event["httpMethod"] != "GET":
         raise Exception(f"getAllItems only accept GET method, you tried: {event.httpMethod}")
 
